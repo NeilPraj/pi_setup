b79ec3683c9f5babd34f2ebb453dde4d546055e0
neilpraj <neilpraj@gmail.com>
2025-12-10 01:21:45 -0500

    added line highliting

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@
 -----------------------------------------------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
